e438365b06eacbaba47b6e0d15803e3c4a13e7bf
naughtymor <znaknikita.it@gmail.com>
2022-10-11 12:28:03 +0300

    splitting and import/export comomponents into folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './Header.js';
+import Technologies from './Technologies.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          I am sure that I will become a <code>programmer</code>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Technologies />
     </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       I am sure that I will become a <code>programmer</code>
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
+
+
+
+
 
 export default App;
