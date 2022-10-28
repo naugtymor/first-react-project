3125a903f714d0836f3ee917304edf0be1ced03f
naughtymor <znaknikita.it@gmail.com>
2022-10-28 19:09:49 +0300

    test.1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Dialogs from './components/Dialogs/Dialogs';
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
@@ -13,6 +12,7 @@ import DialogsContainer from "./components/Dialogs/DialogsContainer";
 
 
 const App = (props) => {
+
     return (
         <BrowserRouter>
             <div className={'app-wrapper'}>
