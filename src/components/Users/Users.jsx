f2f305fa7181c58a8318ab96b7449fee134ecabd
naughtymor <znaknikita.it@gmail.com>
2022-11-01 16:19:35 +0300

    ÑÑside effect problem

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,17 +4,19 @@ import axios from "axios";
 import userPhoto from '../../assets/images/user.png'
 
 let Users = (props) => {
-
-    if (props.users.length === 0) {
-        axios
-            .get("https://social-network.samuraijs.com/api/1.0/users")
-            .then(response => {
-                props.setUsers(response.data.items)
-            });
+    let getUsers = () => {
+        if (props.users.length === 0) {
+            axios
+                .get("https://social-network.samuraijs.com/api/1.0/users")
+                .then(response => {
+                    props.setUsers(response.data.items)
+                });
+        }
     }
 
     return (
         <div className={s.usersList}>
+            <button onClick={getUsers}>Get Users</button>
             {
                 props.users.map((u, index) => <div key={index}>
                     <span>
