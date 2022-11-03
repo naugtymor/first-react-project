70fb74886d6173de692c84ccba05634b2cf6a4ff
naughtymor <znaknikita.it@gmail.com>
2022-11-03 22:07:07 +0300

    first stage of pagination

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,18 +5,28 @@ import userPhoto from '../../assets/images/user.png'
 
 class Users extends React.Component {
 
-    constructor(props) {
-        super(props);
-            axios
-                .get("https://social-network.samuraijs.com/api/1.0/users")
-                .then(response => {
-                    this.props.setUsers(response.data.items)
-                });
+    componentDidMount() {
+        axios
+            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items)
+            });
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+
+        let pages = [];
+        for (let i=1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return (
             <div className={s.usersList}>
+                <div className={s.pagesSelector}>
+                    {pages.map(p => <span className={this.props.currentPage === p && s.selectedPage}>{p}</span>)}
+                </div>
                 {
                     this.props.users.map((u, index) => <div key={index}>
                     <span>
