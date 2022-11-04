f18131fa0a43b8b958ed5fe0f311492cf89b2fef
naughtymor <znaknikita.it@gmail.com>
2022-11-04 19:34:10 +0300

    refactoring Users (make the component dumb)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,64 +1,44 @@
 import React from "react";
-import s from './Users.module.css'
-import axios from "axios";
-import userPhoto from '../../assets/images/user.png'
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/user.png";
 
-class Users extends React.Component {
+let Users = (props) => {
 
-    componentDidMount() {
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-                this.props.setTotalUsersCount(response.data.totalCount)
-            });
+    let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let pages = [];
+    for (let i = 1; i <= pagesCount; i++) {
+        pages.push(i);
     }
 
-    onPageChange = (pageNumber) => {
-        this.props.setCurrentPage(pageNumber)
-        axios
-            .get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items)
-            });
-    }
+    let currentPage = props.currentPage;
+    let currentPageFirst = ((currentPage - 5) < 0 ? 0 : currentPage - 5);
+    let currentPageLast = currentPage + 4;
+    let slicedPages = pages.slice(currentPageFirst, currentPageLast);
 
-    render() {
-
-        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
-
-        let pages = [];
-        for (let i=1; i <= pagesCount; i++) {
-            pages.push(i);
-        };
-
-        let currentPage = this.props.currentPage;
-        let currentPageFirst = ((currentPage - 5) < 0 ? 0 : currentPage - 5);
-        let currentPageLast = currentPage + 4;
-        let slicedPages = pages.slice(currentPageFirst, currentPageLast);
-
-        return (
-            <div className={s.usersList}>
-                <div className={s.pagesSelector}>
-                    {slicedPages.map(p => <span className={this.props.currentPage === p && s.selectedPage}
-                                          onClick={(e) => {
-                                              this.onPageChange(p)
-                                          }}>{p + " "}</span>)}
-                    ...
-                </div>
-                {
-                    this.props.users.map((u, index) => <div key={index}>
+    return (
+        <div className={s.usersList}>
+            <div className={s.pagesSelector}>
+                {slicedPages.map(p => <span className={props.currentPage === p && s.selectedPage}
+                                            onClick={() => {
+                                                props.onPageChange(p)
+                                            }}>{p + " "}</span>)}
+                ...
+            </div>
+            {
+                props.users.map((u, index) => <div key={index}>
                     <span>
                         <div>
-                            <img className={s.userImg} src={u.photos.small != null ? u.photos.small : userPhoto}/>
+                            <img className={s.userImg} src={u.photos.small != null ? u.photos.small : userPhoto}
+                                 alt={'description'}/>
                         </div>
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
-                                    this.props.unfollow(u.id)
+                                    props.unfollow(u.id)
                                 }}>Unfollow</button>
                                 : <button onClick={() => {
-                                    this.props.follow(u.id)
+                                    props.follow(u.id)
                                 }}>Follow</button>
                             }
                         </div>
@@ -73,11 +53,11 @@ class Users extends React.Component {
                             <div>{"u.location.city"}</div>
                         </span>
                     </span>
-                    </div>)
-                }
-            </div>
-        )
-    }
+                    </div>
+                )
+            }
+        </div>
+    )
 }
 
 export default Users;
