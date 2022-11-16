2f39579a4a21693ee6f2ef1305fdc13c957365ca
naughtymor <znaknikita.it@gmail.com>
2022-11-16 21:02:51 +0300

    editmode to profile status

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -9,14 +10,11 @@ const ProfileInfo = (props) => {
 
     return (
         <div>
-            <div>
-                <img src='https://i.redd.it/a0ez44uhhx911.jpg' alt={'description of image'}/>
-            </div>
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large}/>
                 <h2>{props.profile.fullName}</h2>
-                <div style={{color: "white"}}>Status: {props.profile.aboutMe} </div>
-                    <div>Searching for a job: {props.profile.lookingForAJobDescription}</div>
+                <ProfileStatus status={'Hello madafaka'}/>
+                <div>Searching for a job: {props.profile.lookingForAJobDescription}</div>
             </div>
         </div>
     )
---
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import s from './ProfileInfo.module.css';
+
+
+class ProfileStatus extends React.Component {
+
+    state = {
+        editMode: false,
+    }
+
+    changeEditMode = () => {
+        this.setState({editMode: !this.state.editMode})
+    }
+
+    render() {
+        return (
+            <div>
+                {this.state.editMode
+                    ? <div><input autoFocus onBlur={this.changeEditMode} value={this.props.status}/></div>
+                    : <div><span onDoubleClick={this.changeEditMode}>{this.props.status}</span></div>
+                }
+            </div>
+        )
+    }
+}
+
+export default ProfileStatus;
