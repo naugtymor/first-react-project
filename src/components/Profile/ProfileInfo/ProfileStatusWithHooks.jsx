f046be864c500cf28964e9330726c235172e1b28
naughtymor <znaknikita.it@gmail.com>
2022-12-01 22:42:50 +0300

    a complete copy class component to the functional(status)

--- src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatusWithHooks.jsx
@@ -5,21 +5,33 @@ import s from './ProfileInfo.module.css';
 const ProfileStatusWithHooks = (props) => {
     console.log("render")
     let [editMode, setEditMode] = useState(false)
+    let [status, setStatus] = useState(props.status)
 
+    const activateEditMode = () => {
+        setEditMode(true)
+    }
 
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateStatus(status)
+    }
+
+    const onStatusChange = (e) => {
+        setStatus(e.currentTarget.value)
+    }
 
     return (
         <div>
             {!editMode &&
                 <div>
                     Status:
-                    <span >{props.status}</span>
+                    <span onDoubleClick={activateEditMode}>{status}</span>
                 </div>
             }
             {editMode &&
                 <div>
                     Status:
-                    <input autoFocus />
+                    <input autoFocus onBlur={deactivateEditMode} onChange={onStatusChange} value={status}/>
                 </div>
             }
         </div>
