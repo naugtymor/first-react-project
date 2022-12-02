19e9b5a44b87d134c77c5045f650ab634080c661
naughtymor <znaknikita.it@gmail.com>
2022-12-03 00:13:21 +0300

    fix jest dependencies in package.json

--- src/redux/profile-reducer.test.js
+++ src/redux/profile-reducer.test.js
@@ -1,21 +1,21 @@
 import profileReducer, {addPostActionCreator} from "./profile-reducer";
 
-test('new post should be added', () => {
+it('new post should be added correctly', () => {
     let action = addPostActionCreator("Znak Nikita");
 
-    let initialState = {
+    let state = {
         posts: [
             {id: 1, message: 'I am learning React', likesCount: '15'},
-            {id: 2, message: 'Its my first post', likesCount: '23'},
+            {id: 2, message: 'It is my first post', likesCount: '23'},
             {id: 3, message: 'Now a know mapping', likesCount: '40'},
             {id: 4, message: 'Cococo', likesCount: '2'},
             {id: 5, message: 'Ole Ole', likesCount: '5'},
         ]
-    }
+    };
 
-    let newState = profileReducer(initialState,action);
+    let newState = profileReducer(state,action);
 
-    expect(newState.posts.length).toBe(5)
-    // expect(newState.posts[4].message).toBe("Znak Nikita");
+    expect(newState.posts.length).toBe(6)
+    expect(newState.posts[5].message).toBe("Znak Nikita");
 
 });
