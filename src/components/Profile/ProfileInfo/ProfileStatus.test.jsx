d72ef70ec5d8d1522d92c998c5906bcc3eab6001
naughtymor <znaknikita.it@gmail.com>
2022-12-08 23:18:31 +0300

    download react-test-renderer, tests for ProfileStatus

---
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -0,0 +1,22 @@
+import {create} from "react-test-renderer";
+import ProfileStatus from "./ProfileStatus";
+
+describe("ProfileStatusWithHooks component", () => {
+    test("status from props should be in the state", () => {
+        const component = create(<ProfileStatus status={"HAHAHA"}/>)
+        const instance = component.getInstance();
+        expect(instance.state.status).toBe("HAHAHA")
+    });
+    test("after creation span should be displayed with correct status", () => {
+        const component = create(<ProfileStatus status={"HAHAHA"}/>)
+        const root = component.root;
+        let span = root.findByType("span");
+        expect(span.length).toBe(1);
+    });
+    test("after creation span should contains with correct status", () => {
+        const component = create(<ProfileStatus status={"HAHAHA"}/>)
+        const root = component.root;
+        let span = root.findByType("span");
+        expect(span.innerText).toBe("HAHAHA");
+    });
+});
