636d79e9c03819bf9bf3419882f5070427119e33
naughtymor <znaknikita.it@gmail.com>
2023-01-08 13:04:14 +0300

    new jest tests to ProfileStatus

--- src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.test.jsx
@@ -7,16 +7,32 @@ describe("ProfileStatusWithHooks component", () => {
         const instance = component.getInstance();
         expect(instance.state.status).toBe("HAHAHA")
     });
-    test("after creation span should be displayed with correct status", async () => {
+    test("after creation <input> shouldn't be displayed", () => {
         const component = create(<ProfileStatus status={"HAHAHA"}/>)
         const root = component.root;
-        let span = await root.findByType("span");
-        expect(span.length).toBe(1);
+        expect(  () => {
+            let input = root.findByType("input");
+        }).toThrow();
     });
-    test("after creation span should contains with correct status", async () => {
+    test("after creation <span> should be displayed with correct status", () => {
         const component = create(<ProfileStatus status={"HAHAHA"}/>)
         const root = component.root;
-        let span = await root.findByType("span");
-        expect(span.innerText).toBe("HAHAHA");
+        let span = root.findByType("span");
+        expect(span.children[0]).toBe("HAHAHA");
+    });
+    test("input should be displayed in editMode instead of span", () => {
+        const component = create(<ProfileStatus status={"HAHAHA"}/>)
+        const root = component.root;
+        let span = root.findByType("span");
+        span.props.onDoubleClick();
+        let input = root.findByType("input");
+        expect(input.props.value).toBe("HAHAHA");
+    });
+    test("callback should be called", () => {
+        const mockCallback = jest.fn()
+        const component = create(<ProfileStatus status={"HAHAHA"} updateStatus={mockCallback}/>)
+        const instance = component.getInstance();
+        instance.deactivateEditMode();
+        expect(mockCallback.mock.calls.length).toBe(1);
     });
 });
