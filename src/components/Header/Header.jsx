32602ca46823e481454e19c4889e21b98eeec233
naughtymor <znaknikita.it@gmail.com>
2023-01-31 15:39:31 +0300

    bug fix with rotating on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,17 +4,16 @@ import {NavLink} from "react-router-dom";
 
 const Header = (props) => {
     return (
-        <header className={s.header}>
-            {/*<img*/}
-            {/*    src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Twitch_Glitch_Logo_Purple.svg/1756px-Twitch_Glitch_Logo_Purple.svg.png'*/}
-            {/*    alt={'description of image'}/>*/}
+        <div className={s.header}>
             <div className={s.loginBlock}>
-                {props.isAuth
-                    ? <div className={s.userName}>{props.login} - <button onClick={props.logout}>Log out</button></div>
-                    : <NavLink to={'/login'}>Login</NavLink>
-                }
+                <p className={s.xxx}>
+                    {props.isAuth
+                        ? <div className={s.userName}>{props.login} - <button onClick={props.logout}>Log out</button></div>
+                        : <NavLink to={'/login'}>Login</NavLink>
+                    }
+                </p>
             </div>
-        </header>
+        </div>
     )
 }
 
